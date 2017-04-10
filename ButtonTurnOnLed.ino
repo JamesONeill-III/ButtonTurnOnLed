61c94fc2226fa514ac42e9bc5d2594c84b107db6
JamesONeill-III <joneill313@Hotmail.com>
2017-04-09 20:09:25 -0700

    Add files via upload

---
+++ ButtonTurnOnLed.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13;
+cont int inputPin = 2;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() 
+{
+  int val = digitalRead(inputPin);
+  if (val == HIGH)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledpin, LOW);
+  }
+}
